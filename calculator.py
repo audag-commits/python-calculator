5f973de413bbda773bf12f4bac7f2d8e4cdcb125
audag-commits <audagoonasekera@gmail.com>
2025-10-18 08:53:07 +0530

    Added Power Function

--- calculator.py
+++ calculator.py
@@ -14,14 +14,18 @@ def divide (x,y):
         return "Error! Division by Zero"
     return x/y
 
+def power (x,y):
+    return x**y
+
 print("Basic Calculator")
 print("Select Operation")
 print("1. Add")
 print("2. Subtract")
 print("3. Multiply")
 print("4. Divide")
+print("5. Power") 
 
-choice = input ("Enter Operation 1/2/3/4: ")
+choice = input ("Enter Operation 1/2/3/4/5: ")
 
 num1 = float(input("Enter First Number: "))
 num2 = float(input ("Enter Second Number: "))
@@ -34,5 +38,7 @@ elif choice == '3':
     print("Result:", multiply(num1, num2))
 elif choice == '4':
     print("Result:", divide(num1, num2))
+elif choice == '5':
+    print("Result:", power(num1, num2))
 else:
     print("Invalid Input")
